ebb8590283a641e70f11e8d491ca410a14691a82
Eugene Kolesnyk <eugene.kolesnyk@devit-team.com>
2020-07-30 16:28:02 +0200

    Added builded version

---
+++ build/precache-manifest.294bbab915168b4cd8481ee7f2363745.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21322b9642b87aaec96006df13c641e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46ab67eba513ce3f3e3b",
+    "url": "/static/css/main.331273f0.chunk.css"
+  },
+  {
+    "revision": "238acd998e2a30dc24fc",
+    "url": "/static/js/2.5d92fda7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5d92fda7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46ab67eba513ce3f3e3b",
+    "url": "/static/js/main.ca47f947.chunk.js"
+  },
+  {
+    "revision": "9599b3a51349788d743c",
+    "url": "/static/js/runtime-main.7cbb5edb.js"
+  }
+]);
